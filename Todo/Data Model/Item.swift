c63b05c580c29af64436bb7b6689f876b96e2ac4
Mauricio Mercado <mau@maumercado.com>
2018-07-30 17:18:26 -0700

    Convert to MVC for clearer read and created a model for each item

---
+++ Todo/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todo
+//
+//  Created by Mauricio Mercado on 2018-07-30.
+//  Copyright © 2018 Mauricio Mercado. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
